ea9c959d18684d004eeabecc8bd93b2881ac32e8
osiyeon <osiyun0313@gmail.com>
2022-06-11 21:05:45 +0900

    config 설정

--- next.config.js
+++ next.config.js
@@ -3,15 +3,14 @@ module.exports = {
   images: {
     domains: ["https://sharing-giving.s3.ap-northeast-2.amazonaws.com/"]
   },
+
   async rewrites() {
     return [
       {
         source: "/:path*",
         destination:
-          process.env.NODE_EN === "production"
-            ? encodeURI(
-                "http://ec2-15-164-87-12.ap-northeast-2.compute.amazonaws.com:8080"
-              )
+          process.env.NODE_ENV === "production"
+            ? "http://ec2-15-164-87-12.ap-northeast-2.compute.amazonaws.com:8080/:path*"
             : "http://localhost:8080/:path*" // Proxy to Backend
       }
     ];
